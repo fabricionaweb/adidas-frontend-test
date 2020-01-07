0415d3f3f57b0114a28410a7510aabd34d0f3f4a
Fabricio Augusto <fabricionaweb@gmail.com>
2020-01-07 17:29:44 +0000

    Move App component

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Header } from '../Header';
+
+export function App() {
+  return (
+    <main>
+      <Header lead="Sign up form" title="Get more information" />
+    </main>
+  );
+}
+
+export default App;
