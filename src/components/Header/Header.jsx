7eb1e0b92343325c9515bb40da134e442fa59fa0
Fabricio Augusto <fabricionaweb@gmail.com>
2020-01-07 17:29:23 +0000

    Create heading component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Header.module.css';
+
+export function Header({ lead, title }) {
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.title}>
+        {lead && <p>{lead}</p>}
+
+        {title}
+      </h1>
+    </header>
+  );
+}
+
+Header.defaultProps = {
+  lead: null,
+};
+
+Header.propTypes = {
+  lead: PropTypes.string,
+  title: PropTypes.string.isRequired,
+};
+
+export default Header;
